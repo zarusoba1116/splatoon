09ba54e2d6937ea2c4a37e9d766344abd34c39f6
zarusoba1116 <91121948+zarusoba1116@users.noreply.github.com>
2022-11-12 07:38:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,8 +30,8 @@ async def embed1(ctx):
                           color=0x53a9e9, # フレーム色指定(今回は緑)
                           description="メンバーは、すべてのルールに従うことが求められます。\nこれに従わない場合は、適切な措置が取られる可能性があります。", inline = False)
     embed1.add_field(name="🔹 **ベルリン行動規範**",value="このサーバーでは「ベルリン行動規範」と呼ばれるインターネットコミュニティに最適化されたルールを適用させて頂いています。\n`🔗`[ベルリン行動規範](https://berlincodeofconduct.org/ja/)", inline = False) # フィールドを追加。
-    embed1.add_field(name="🔹 **Discordの利用規約とコミュニティ・ガイドライン、も遵守されます**",value="`🔗`[利用規約](https://discord.com/terms)`🔗`[コミュニティ・ガイドライン](https://discord.com/guidelines)", inline = False) # フィールドを追加。
-    embed1.add_field(name="🔹 **トピックを守り、チャンネルを正しく使って下さい**",value="<#899620660937101332>と、トピックに沿ってチャンネルを使用して下さい", inline = False) # フィールドを追加。
+    embed1.add_field(name="🔹 **Discordの利用規約とコミュニティ・ガイドライン、も遵守されます**",value="`🔗`[利用規約](https://discord.com/terms)\n`🔗`[コミュニティ・ガイドライン](https://discord.com/guidelines)", inline = False) # フィールドを追加。
+    embed1.add_field(name="🔹 **トピックを守り、チャンネルを正しく使って下さい**",value="トピックに沿ってチャンネルを正しく使用して下さい", inline = False) # フィールドを追加。
     embed1.add_field(name="🔹 **いかなる種類の広告、自己宣伝、一般的な勧誘の禁止**",value="第三者のサービスを宣伝しないでください。", inline = False) # フィールドを追加。
     await ctx.send(embed=embed1) # embedの送信には、embed={定義したembed名}
 
